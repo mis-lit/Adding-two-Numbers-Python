062b1cc100eb687ae4396e31fad2851973ddb13a
Srikanth Pinisetti <65670179+therealsrikanth@users.noreply.github.com>
2020-09-30 11:00:10 +0530

    Update subtwonumbers.py

--- subtwonumbers.py
+++ subtwonumbers.py
@@ -2,16 +2,11 @@
   
 number1 = input("First number: ") 
 number2 = input("\nSecond number: ") 
-number3 = input("Third number: ")
-number4 = input("Fourth number: ")
-  
-# Substracting two numbers 
+
+# Mul two numbers 
 # User might also enter float numbers 
-sub1 = float(number1) - float(number2) 
-sub2 = float(number1) - float(number3) 
-sub3 = float(number3) - float(number4)   
-# Display the sub 
+mul = float(number1) * float(number2) 
+
+# Display the mul
 # will print value in float 
-print(sub1)
-print(sub2)
-print(sub3)
+print(mul)
