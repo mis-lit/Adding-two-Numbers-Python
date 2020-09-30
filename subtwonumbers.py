44863652184557782c6a7b1d63147191e6c02ab9
Srikanth Pinisetti <65670179+therealsrikanth@users.noreply.github.com>
2020-09-30 08:55:35 +0530

    Added 4th Input

--- subtwonumbers.py
+++ subtwonumbers.py
@@ -3,13 +3,15 @@
 number1 = input("First number: ") 
 number2 = input("\nSecond number: ") 
 number3 = input("Third number: ")
+number4 = input("Fourth number: ")
   
 # Substracting two numbers 
 # User might also enter float numbers 
 sub1 = float(number1) - float(number2) 
 sub2 = float(number1) - float(number3) 
-  
+sub3 = float(number3) - float(number4)   
 # Display the sub 
 # will print value in float 
 print(sub1)
 print(sub2)
+print(sub3)
