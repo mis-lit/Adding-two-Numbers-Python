e50ad471cae99442950e1f503384bfa52a20bedd
Srikanth Pinisetti <65670179+therealsrikanth@users.noreply.github.com>
2020-09-30 08:48:09 +0530

    Create subtwonumbers.py

---
+++ subtwonumbers.py
@@ -0,0 +1,12 @@
+# Program to Sub two numbers 
+  
+number1 = input("First number: ") 
+number2 = input("\nSecond number: ") 
+  
+# Substracting two numbers 
+# User might also enter float numbers 
+sub = float(number1) - float(number2) 
+  
+# Display the sub 
+# will print value in float 
+print(sub)) 
